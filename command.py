8ace0127ba08f295ea5f0ea6eb96f03b13e08daa
chembeu <simon.shanga11@gmail.com>
2025-01-13 10:21:40 +0300

    cmd

---
+++ command.py
@@ -0,0 +1,11 @@
+import cmd
+class HelloWorld(cmd.Cmd):
+    """simple command processor example"""
+    prompt = "chembeu > "
+    def do_greet(self, line):
+        print ("hello")
+    def do_EOF(self, line):
+        return True
+
+if __name__ == '__main__':
+    HelloWorld().cmdloop()
