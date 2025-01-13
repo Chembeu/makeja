6b91d6ecf33e1b6f4e60611da3950aaf7678dc75
chembeu <simon.shanga11@gmail.com>
2025-01-13 11:56:56 +0300

    exit

--- command.py
+++ command.py
@@ -2,8 +2,13 @@ import cmd
 class HelloWorld(cmd.Cmd):
     """simple command processor example"""
     prompt = "chembeu > "
+    intro = "Welcome to chembeu shell"
     def do_greet(self, line):
         print ("hello")
+    def do_exit(self,arg):
+        """Exiting the terminal"""
+        print("Goodbye")
+        return True
     def do_EOF(self, line):
         return True
 
